bb165592545c516e3560b45effe58bd32377fab0
danil <daniachmetshin@gmail.com>
2024-12-08 18:42:23 +0200

    new getAllReviewsByMovieId

--- server/models/reviewModel.js
+++ server/models/reviewModel.js
@@ -1,12 +1,20 @@
 import { pool } from '../db.js';
 
-export const postReview = async (userId, movieId, rating, info) => {
+export const getAllReviewsByMovieId = async (movie_id) => {
     const query = `
-        INSERT INTO reviews (user_id, movie_id, rating, info)
-        VALUES ($1, $2, $3, $4)
+        SELECT * FROM reviews
+        WHERE movie_id = $1;
+    `;
+    return pool.query(query, [movie_id]);
+};
+
+export const postReview = async (userId, email, movieId, rating, info) => {
+    const query = `
+        INSERT INTO reviews (user_id, email, movie_id, rating, info)
+        VALUES ($1, $2, $3, $4, $5)
         RETURNING *;
     `;
-    return pool.query(query, [userId, movieId, rating, info]);
+    return pool.query(query, [userId, email, movieId, rating, info]);
 };
 
 export const getReviewByUserAndMovie = async (userId, movieId) => {
